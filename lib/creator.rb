3b106d883123d7c04c3e4f11c569b1868d423ee8
clairedotcom <claireacarson@gmail.com>
2021-07-13 12:56:47 -0400

    Refactored color display and code to be numbers instead of colors

--- lib/creator.rb
+++ lib/creator.rb
@@ -1,5 +1,5 @@
 class Creator
     def make_code
-        code = 4.times.map {|value| rand(6)}
+        code = 4.times.map {|value| rand(6).to_s}
     end    
 end
--- lib/game.rb
+++ lib/game.rb
@@ -2,15 +2,15 @@ require_relative "creator.rb"
 require_relative "guesser.rb"
 
 class Game
-    COLORS = ["blue","green","yellow","orange","pink","red"]
+    #COLORS = ["blue","green","yellow","orange","pink","red"]
 
     def initialize
         @creator = Creator.new
     end    
 
     def play
-        code = code_to_colors(@creator.make_code)
-        display(code)
+        #code = code_to_colors(@creator.make_code)
+        code = @creator.make_code
         guess = Array.new(4)
         
         until guess == code do
@@ -25,8 +25,10 @@ class Game
     end
     
     def solicit_guess
-        puts "Please enter your guess (one by one): "
-        guess = Array.new(4) {gets.chomp}
+        puts "Please enter your guess: "
+        guess = gets.chomp
+        guess.split("")
+        #guess = Array.new(4) {gets.chomp}
     end  
     
     def check_guess(guess,code)
@@ -44,20 +46,22 @@ class Game
     end
     
     def display(array)
-        array.each do |item| 
-            print map_colors(item)
-        end
-        #puts "#{map_colors(array[0])} | #{array[1]} | #{array[2]} | #{array[3]}"
+        #array.each do |item| 
+        #    print map_colors(item)
+        #end
+        puts "#{map_colors(array[0])} | #{map_colors(array[1])} | #{map_colors(array[2])} | #{map_colors(array[3])}"
     end
     
     def map_colors(key)
         {
-            "blue" => "\e[34m blue \e[0m",
-            "green" => "\e[32m green \e[0m",
-            "yellow" => "\e[33m yellow \e[0m",
-            "orange" => "\e[47m orange \e[0m",
-            "pink" => "\e[35m pink \e[0m",
-            "red" => "\e[31m red \e[0m"
+            #"blue" => "\e[34m blue \e[0m",
+            #"green" => "\e[32m green \e[0m",
+            #"yellow" => "\e[33m yellow \e[0m",
+            #"orange" => "\e[47m orange \e[0m",
+            #"pink" => "\e[35m pink \e[0m",
+            "empty" => "\e[31m empty \e[0m",
+            "black" => "\e[40m black \e[0m",
+            "white" => "\e[7m white \e[27m"
         }[key]
     end    
 end
